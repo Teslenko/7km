401e33897d1e367af879ed6bdee59bd0af3c0426
Evgeniy Ivanov <30300100+Evgiv08@users.noreply.github.com>
2018-04-28 11:00:15 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+$('.slider').slick({
+  dots: true,
+  autoplay: true,
+  arrows: false
+});
+function initMap() {
+  var uluru = { lat: 46.440775, lng: 30.638143 };
+  var map = new google.maps.Map(document.getElementById('googlemaps'), {
+    zoom: 17,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
+//# sourceMappingURL=script.js.map
